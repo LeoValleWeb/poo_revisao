d56b7641f527bcca13a2d5602e26902676333f27
LeoValleWeb <leomartinsvalle@gmail.com>
2023-10-25 16:54:34 +0000

    save

---
+++ Aluno.java
@@ -0,0 +1,42 @@
+public class Aluno{
+    private String nome;
+    private double media;
+    private int numFaltas;
+    private int numAulas;
+
+    public double getMedia() {
+        return media;
+    }
+    public void setMedia(double media) {
+        this.media = media;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumAulas() {
+        return numAulas;
+    }
+    public void setNumAulas(int numAulas) {
+        this.numAulas = numAulas;
+    }
+
+    public int getNumFaltas() {
+        return numFaltas;
+    }
+    public void setNumFaltas(int numFaltas) {
+        this.numFaltas = numFaltas;
+    }
+
+
+    public Aluno(){
+
+    }
+    public void cadastrar(String nome){
+        
+    }
+}
